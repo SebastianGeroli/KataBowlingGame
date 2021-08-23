3650302113e9a4fe96deec26f25fcc3f5069749a
SebastianGeroli <sebastiangeroli@outlook.com>
2021-08-23 20:48:43 -0300

    Starting with SessionGame

--- Assets/Scripts/Models/SessionGame.cs
+++ Assets/Scripts/Models/SessionGame.cs
@@ -3,9 +3,20 @@
 public class SessionGame
 {
     private Turn[] _turns = new Turn[10];
-    
+    public int Score => CalculateScore();
+
+    private int CalculateScore()
+    {
+        return 15;
+    }
+
     public IEnumerable<Turn> GetTurns()
     {
         return _turns;
+    }
+
+    public void Shoot(int amount)
+    {
+        //
     }
 }
--- Assets/Scripts/Tests/SessionGameShould.cs
+++ Assets/Scripts/Tests/SessionGameShould.cs
@@ -3,19 +3,44 @@ using NUnit.Framework;
 
 public class SessionGameShould
 {
+    private SessionGame _session;
+
+    [SetUp]
+    public void Before()
+    {
+        _session = new SessionGame();
+    }
 
     [Test]
     public void Have_10_turns_to_be_valid()
     {
-        //Given
-        var session = new SessionGame();
         //When
-        var turnsCount = session.GetTurns().ToList().Count();
-        
+        var turnsCount = _session.GetTurns().ToList().Count();
+
         //Then
         Assert.AreEqual(10, turnsCount);
     }
-    //
-    // [Test]
-    // public void 
+
+    [Test]
+    public void ScoreShouldReturn15()
+    {
+        
+        //When
+        _session.Shoot(0);
+        _session.Shoot(10);
+        _session.Shoot(5);
+        //Then
+        Assert.AreEqual(15, _session.Score);
+    }
+
+    [Test]
+    public void ScoreShouldReturn20()
+    {
+        //When
+        _session.Shoot(0);
+        _session.Shoot(10);
+        _session.Shoot(10);
+        //Then
+        Assert.AreEqual(20, _session.Score);
+    }
 }
