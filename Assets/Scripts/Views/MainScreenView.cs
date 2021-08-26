c8a73c73bfd2d6cd0914f378add41a33fc72673f
Juan Sebastián Martin <38918218+sebamartin10@users.noreply.github.com>
2021-08-26 18:23:45 -0300

    Added Scripts of Views

---
+++ Assets/Scripts/Views/MainScreenView.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MainScreenView : MonoBehaviour, IMainScreenView
+{
+    MainScreenPresenter mainScreenPresenter;
+
+
+    
+    public void Shoot() {
+        
+    }
+}
